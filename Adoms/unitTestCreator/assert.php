68d8afa30b6e64303d59df2228874d3cd9ba7fed
saasey <48605836+saasey@users.noreply.github.com>
2020-07-28 14:17:17 -0400

    Update assert.php

--- Adoms/unitTestCreator/assert.php
+++ Adoms/unitTestCreator/assert.php
@@ -114,7 +114,7 @@ function func_change (t) {
         if (j == 1)
             h = h + x[i]; 
     }
-    document.getElementById("code").value = "\<\?php\n\nnamespace " + m.substr(0,m.lastIndexOf('\\')) + ";\n\ninclude '\\"+ m + "';\n\n" + f + "" + m.substr(m.lastIndexOf("\\")+1) + "Test {\n" + "\tpublic function testCheckForFunction" + func + "() \n\t{\n\t\t\$obj = new " + m + "();\n\t\t\$obj->" + func + "();\n\t}" + h.substr(1,h.length-6) + "\n}\n?>";
+    document.getElementById("code").value = "\<\?php\n\nnamespace " + m.substr(0,m.lastIndexOf('\\')) + ";\n\ninclude '\\"+ m + ".php';\n\n" + f + "" + m.substr(m.lastIndexOf("\\")+1) + "Test {\n" + "\tpublic function testCheckForFunction" + func + "() \n\t{\n\t\t\$obj = new " + m + "();\n\t\t\$obj->" + func + "();\n\t}" + h.substr(1,h.length-6) + "\n}\n?>";
 }
 ['click', 'touch', 'tap'].forEach(function(e) {
     window.addEventListener(e, function(ev) {
