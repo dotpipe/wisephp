ffb9b7279fe389a2a6cb985d3c243116dc080dd0
saasey <48605836+saasey@users.noreply.github.com>
2020-08-03 15:55:35 -0400

    Adoms::Helium v4.0.0

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ file_put_contents("example.txt",":):):):)");
 ?>
 <script src="./adoms/src/routes/pipes.js"></script>
 <span id="hed" style="width:100%"><br>
-Adoms::Helium v3.0.0 - <a href="http://www.github.com/swatchphp">GitHub</a> +
+Adoms::Helium v4.0.0 - <a href="http://www.github.com/swatchphp">GitHub</a> +
 <a id="wiki-link" method="GET" insert="red" ajax="example.txt">Wiki</a> +
 <i id="donate" redirect="follow" method="POST" goto="https://www.paypal.com/cgi-bin/webscr"> Donate + </i>
 <input type="hidden" pipe="donate" class="data-pipe" name="cmd" value="_s-xclick" />
